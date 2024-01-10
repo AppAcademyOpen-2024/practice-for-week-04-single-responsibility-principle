d240bdebad0398925577d9c428a85c067f3ba39b
Dvwhite81 <Dvwhite1981@gmail.com>
2024-01-09 19:41:22 -0500

    complete

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-/******************************** CONSTANTS *********************************/ 
+/******************************** CONSTANTS *********************************/
 const applePieRecipe = [
   { name: "pie crust", cost: 10.00, quantity: 1 },
   { name: "sugar", cost: 3.00, quantity: 0.5 },
@@ -36,17 +36,28 @@ const recipes = {
 function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
   // Find the recipe for the pieType specified
   const recipe = recipes[pieType];
+
+  bakePies(recipe, pieType, pieQuantity);
+
+  const totalCost = getCost(recipe, pieQuantity);
+
+  sellPies(pieQuantity, totalCost, profitMargin);
+}
+
+function bakePies(recipe, pieType, pieQuantity) {
   // Bake the number of pies specified by the pieQuantity
   for (let i = 0; i < pieQuantity; i++) {
     // Print the ingredients for each ingredient in the recipe
-    let combiningMsg = `Combining ingredients for ${pieType}: `
+    let combiningMsg = `Combining ingredients for ${pieType}: `;
     combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
     console.log(combiningMsg);
 
     // Print the nth pie that was baked
     console.log(`Baked pie ${i + 1}!`);
   }
+}
 
+function getCost(recipe, pieQuantity) {
   // Print the cost of each pie based on the cost of each ingredient
   const costOfPie = recipe.reduce((prev, current) => {
     return prev + current.cost;
@@ -54,8 +65,10 @@ function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
   console.log(`Cost per pie: ${costOfPie}`);
 
   // Calculate the total cost of all the pies
-  const totalCost = costOfPie * pieQuantity;
+  return costOfPie * pieQuantity;
+}
 
+function sellPies(pieQuantity, totalCost, profitMargin) {
   // Print the total revenue calculated using the given profitMargin
   const revenue = totalCost * (profitMargin || 1.2);
   console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
